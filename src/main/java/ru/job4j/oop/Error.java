2e8691c28111fcaf5b50a9fbbd73c97448809d0b
PetukhovIS <petukhov.is.96@ya.ru>
2024-06-04 17:54:31 +0600

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+
+     }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.message = message;
+        this.status = status;
+    }
+
+    public void printInfo() {
+        System.out.println("Error: " + active);
+        System.out.println("Status is: " + status);
+        System.out.println("Description: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error error404 = new Error(true, 404, "Not Found");
+        error.printInfo();
+        error404.printInfo();
+    }
+
+}
