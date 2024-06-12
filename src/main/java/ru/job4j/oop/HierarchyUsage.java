331ada7bc7d5f00fc6cfc6b52212b9b6b0a292ee
PetukhovIS <petukhov.is.96@ya.ru>
2024-06-12 18:31:08 +0600

    5.0. Приведение типов. Повышение типа и понижение типа. [#181322]

---
+++ src/main/java/ru/job4j/oop/HierarchyUsage.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+public class HierarchyUsage {
+    public static void main(String[] args) {
+        Car car = new Car();
+        Transport transport = car;
+        Object object = car;
+        Object objectCar = new Car();
+
+    }
+}
