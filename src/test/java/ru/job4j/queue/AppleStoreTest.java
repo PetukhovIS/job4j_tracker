5707c2534f8d986e1fb43d724c0a4623ffc5357d
PetukhovIS <petukhov.is.96@ya.ru>
2024-06-29 14:01:21 +0600

    1. Интерфейс Queue [#504942]

--- src/test/java/ru/job4j/queue/AppleStoreTest.java
+++ src/test/java/ru/job4j/queue/AppleStoreTest.java
@@ -17,7 +17,7 @@ class AppleStoreTest {
         customers.add(new Customer("Iryna", 1250));
         customers.add(new Customer("Elena", 750));
         customers.add(new Customer("Rail", 950));
-        int count = 1;
+        int count = 4;
         AppleStore appleStore = new AppleStore(customers, count);
         String customer = appleStore.getLastHappyCustomer();
         assertThat(customer).isEqualTo("Alexei");
