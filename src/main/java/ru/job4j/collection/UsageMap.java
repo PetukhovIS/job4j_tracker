b7fa014267d59e17a0e292e663e67a67c805992e
PetukhovIS <petukhov.is.96@ya.ru>
2024-07-01 10:38:11 +0600

    0. Map, HashMap [#212652 #502867]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
+        map.put("Petukhov.IS.96@yandex.ru", "Petukhov");
         map.put("Petukhov.IS.96@yandex.ru", "Ilya Petukhov");
+        map.put("Petukhov.IS.96@gmail.com", "Ilya Cashtanov");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
